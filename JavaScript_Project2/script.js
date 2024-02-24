e5671b9bb11cb3931d3d4f4adab7d8f8b12affc8
Alperen ATASEVEN <alperenataseven.3@gmail.com>
2024-02-24 15:56:50 +0300

    new project

---
+++ JavaScript_Project2/script.js
@@ -0,0 +1,4 @@
+const progress = document.querySelector("#progress")
+const circles = document.querySelectorAll(".circle")
+const prev = document.querySelector("#prev")
+const next = document.querySelector("#next") 
